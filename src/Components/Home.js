7f40219f39d258295666d11304ad66faae480a0a
ohsuji <tnwltnwl93@naver.com>
2023-02-24 18:01:08 +0900

    Product Update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,17 +9,44 @@ function Home() {
         <img className="Home-image" src="https://m.media-amazon.com/images/I/71z8youNyRL._SX3000_.jpg" alt="Home-image"/>
 
         <div className="Home-row">
-          <Product/>
+          <Product 
+            id="1234" 
+            title="mac book pro 14" 
+            price={2700000} 
+            image="https://www.apple.com/kr/macbook-pro/ah/images/overview/hero_13__d1tfa5zby7e6_large_2x.jpg" 
+            rating={5}/>
         </div>
 
         <div className="Home-row">
-          <Product/>
-          
+          <Product 
+              id="2345" 
+              title="Pillow Plush Toy" 
+              price={37000}
+              image="https://m.media-amazon.com/images/I/61y-X5OGtnS._AC_UL800_FMwebp_QL65_.jpg" 
+              rating={3}/>
+
+          <Product 
+              id="3456" 
+              title="Nintendo Switch : OLED Model" 
+              price={360000}
+              image="https://m.media-amazon.com/images/I/51yJ+OqktYL._AC_UY436_FMwebp_QL65_.jpg" 
+              rating={4}/>
+
+          <Product 
+              id="4567" 
+              title="Hogwarts Legacy" 
+              price={80000}
+              image="https://m.media-amazon.com/images/I/816nYzwbSOL._AC_UY436_FMwebp_QL65_.jpg" 
+              rating={5}/>
         </div>
 
         <div className="Home-row">
-
-          
+          <Product 
+              id="5678" 
+              title="PlayStation 5" 
+              price={660000}
+              image="https://m.media-amazon.com/images/I/51051FiD9UL._AC_UY436_FMwebp_QL65_.jpg" 
+              rating={2}/>       
         </div>
 
       </div>
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import './Product.css';
 
-function Product() {
+function Product({id, title, image, price, rating}) {
   return (
     <div className="Product">
       <div className="Product-info">
-        <p>제품1</p>
+        <p>{ title }</p>
+
         <p className="Product-price">
           <small>가격</small>
-          <strong>10,000</strong>
+          <strong> {price} </strong>
           <small>원</small>
         </p>
+
         <div className="Product-rating">
-          <p>★</p>
-          <p>★</p>
-          <p>★</p>
-          <p>★</p>
+          { 
+            Array(rating)
+              .fill()
+              .map(() => (
+                <p>★</p>
+              ))
+          }
         </div>
 
-        <img className="Product-image"src="https://www.apple.com/kr/macbook-pro/ah/images/overview/hero_13__d1tfa5zby7e6_large_2x.jpg" alt="Product-image"/>
+        <img className="Product-image" src={image} alt="Product-image"/>
+
         <button className="Product-addBasket">장바구니에 담기</button>
       </div>
     </div>
